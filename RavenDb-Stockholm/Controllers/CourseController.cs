25b2e7a7c087a8eee9ac0e0037e67e49d4b746df
Ayende Rahien <ayende@ayende.com>
2012-05-21 15:01:26 +0200

    Adding courses

---
+++ RavenDb-Stockholm/Controllers/CourseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+using RavenDb_Stockholm.Models;
+using System.Linq;
+
+namespace RavenDb_Stockholm.Controllers
+{
+	public class CourseController : RavenCrudController<Course>
+	{
+		 public ActionResult Create(string name, string instructor, string[] tech)
+		 {
+			 var course = new Course
+			 {
+				 Name = name,
+				 Content = new string('*', 10),
+				 Date = DateTime.Today,
+				 Instructor = instructor,
+				 Location = "Stockholm",
+				 Technologies = tech
+			 };
+			 Session.Store(course);
+
+			 var documentId = Session.Advanced.GetDocumentId(course);
+			 return RedirectToAction("Details", new { id = documentId.Split('/').Last() });
+     
+		 }
+	}
+}
